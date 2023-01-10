cfb6e214254f6fc97e84e85e2402ba25941b40c3
Riya Khandelwal <riyukhandelwal@gmail.com>
2023-01-10 16:02:44 +0530

    Update streamlit_app.py
    adding 2nd food header

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.text(' 🥣 Omega 3 & blueberry oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smothie')
 streamlit.text(' 🐔 Hard-Boiled Free-range egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
