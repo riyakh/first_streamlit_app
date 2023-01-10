a62f760bb674c2cb73ef0d326c0de4a0f49e3d3a
Riya Khandelwal <riyukhandelwal@gmail.com>
2023-01-10 15:58:09 +0530

    Update streamlit_app.py
    added streamlit header
    added streamlit text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smothie')
+streamlit.text('Hard-Boiled Free-range egg')
